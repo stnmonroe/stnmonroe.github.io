eac224c935dcd2b438f4dba9341966b16564681e
stnmonroe <stnmonroe@gmail.com>
2018-08-14 14:22:41 -0500

    portfolioBoxes working appropriately thus far, just finished linking

--- script.js
+++ script.js
@@ -3,6 +3,20 @@ var menuDrawer = document.getElementById('menuDrawer');
 var menuItems = document.getElementsByClassName('menuItemConatiner');
 var lastScrollTop = 0;
 
+//Click anywhere except on menu to close menuDrawer
+document.addEventListener('click', (event) => {
+  const e = event.target || event.srcElement;
+
+  if (e !== menuBall && e !== menuDrawer &&
+      e.parentElement !== menuBall && e.parentElement !== menuDrawer) {
+    menuBall.classList.remove("open");
+    menuDrawer.classList.remove("open");
+    for(var i=0; i < menuItems.length; i++) {
+      menuItems[i].classList.remove("open");
+    }
+  }
+})
+
 window.onload = () => {
   setTimeout( () => {
     menuBall.classList.remove("offScreen");
@@ -37,34 +51,55 @@ var portfolioItems = [
       name: "Witwars",
       src: "img/witwars.png"
     },
-    tech: ["React-Native"],
-    desc: "Mobile application for iOS and Android offering you the chance to compete at hilarity."
+    tech: ["React-Native", "Redux", "Router-Flux", "Node.js", "Express", "Pug", "CSS", "jQuery", "AdMob"],
+    desc: "Sole UI Engineer for mobile app, designer, digital content creator, and developer for web app.",
+    btn: {
+      text: "More Info",
+      name: "witwars"
+    }
   },
   {
     img: {
       name: "Voyager",
       src: "img/voyager.png"
     },
-    tech: ["React-Native"],
-    desc: "Mobile application for iOS and Android offering you the chance to compete at hilarity."
+    tech: ["Wordpress", "HTML", "CSS", "SEO", "Google Ads", "Facebook Ads", "GIMP", "Sketch 3", "Final Cut Pro"],
+    desc: "Designed website and online marketing strategy, plus created most video, written, and graphic content for manufacturing company.",
+    btn: {
+      text: "Visit Site",
+      link: "http://www.mtcvoyager.com"
+    }
   },
   {
     img: {
       name: "Witwars Admin",
       src: "img/witwars_admin.png"
     },
-    tech: ["React-Native"],
-    desc: "Mobile application for iOS and Android offering you the chance to compete at hilarity."
+    tech: ["React", "Redux", "Router", "Node.js", "Express", "Postgres", "Sequelize", "Axios"],
+    desc: "Full stack development of console for Witwars app allowing admin accounts to manage premises.",
+    btn: {
+      text: "More Info",
+      name: "witwars_admin"
+    }
   },
   {
     img: {
       name: "Ottawa Recreation",
       src: "img/ottawa_rec.png"
     },
-    tech: ["React-Native"],
-    desc: "Mobile application for iOS and Android offering you the chance to compete at hilarity."
+    tech: ["Wordpress", "HTML", "CSS"],
+    desc: "As a member of the Ottawa, IL Playgrounds & Recreation Board, I voluntarily built them a new website.",
+    btn: {
+      text: "Visit Site",
+      link: "http://www.ottawarecreation.org"
+    }
   }
 ]
+
+openInNewTab = (url) => {
+  let win = window.open(url, "_blank");
+  win.focus();
+}
 
 createPortfolioBoxes = () => {
   const p = document.getElementById("portfolio");
@@ -77,12 +112,30 @@ createPortfolioBoxes = () => {
       <div class="img2ndHalf" style="background: url(` + pI[i].img.src + `) -125px 0"></div>
       <div class="portfolioInfo">
         <p class="portfolioDesc">` + pI[i].desc + `</p>
-        <div class="portfolioTech"></div>
-        <button class="portfolioBtn">View More</button>
+        <div class="portfolioTechContainer">
+          <div class="portfolioTech"></div>
+        </div>
+        <div class="portfolioBtnContainer">
+          <div class="portfolioBtn">` + pI[i].btn.text + `</div>
+        </div>
       </div>
     `
-    console.log(div)
     p.appendChild(div);
+  }
+
+  //Add links to portfolio info buttons
+  let btns = p.querySelectorAll(".portfolioBtn");
+  for (let j = 0; j < btns.length; j++) {
+    const desc = btns[j].parentElement.parentElement
+                  .querySelector(".portfolioDesc").textContent;
+    const index = portfolioItems.findIndex( p => p.desc === desc );
+    btns[j].addEventListener("click", () => {
+      if (portfolioItems[index].btn.link) {
+        openInNewTab(portfolioItems[index].btn.link);
+      } else {
+        console.log("no link found");
+      }
+    })
   }
 }
 
@@ -90,10 +143,12 @@ createPortfolioBoxes = () => {
 let portfolioBoxes;
 
 const openPortfolioBox = (event) => {
-  var e = event.target || event.srcElement;
+  let e = event.target || event.srcElement;
 
   if (e.classList.contains("portfolioInfo") || e.classList.contains("portfolioDesc") ||
-      e.classList.contains("portfolioTech") || e.classList.contains("portfolioBtn")) {
+      e.classList.contains("portfolioTech") || e.classList.contains("portfolioBtn") ||
+      e.classList.contains("portfolioTechContainer") || e.classList.contains("portfolioTechItem") ||
+      e.classList.contains("portfolioBtnContainer")) {
         return null;
   } else {
 
@@ -103,11 +158,25 @@ const openPortfolioBox = (event) => {
 
     e.children[0].classList.add("open-left");
     e.children[1].classList.add("open-right");
+
+    const tech = e.querySelector(".portfolioTech");
+    if (!tech.querySelector(".portfolioTechItem")) {
+      const desc = e.querySelector(".portfolioDesc").textContent;
+      const index = portfolioItems.findIndex( p => p.desc === desc );
+
+      for (let i = 0; i < portfolioItems[index].tech.length; i++) {
+        let div = document.createElement("DIV");
+        let text = document.createTextNode(portfolioItems[index].tech[i]);
+        div.appendChild(text);
+        div.classList.add("portfolioTechItem");
+        tech.appendChild(div);
+      }
+    }
   }
 }
 
 const closePortfolioBox = (event) => {
-  var e = event.target || event.srcElement;
+  const e = event.target || event.srcElement;
 
   if (e.classList.contains("portfolioInfo") || e.classList.contains("portfolioDesc") ||
       e.classList.contains("portfolioTech") || e.classList.contains("portfolioBtn")) {
@@ -125,8 +194,8 @@ const closePortfolioBox = (event) => {
 
 setTimeout(() => {
   portfolioBoxes = document.querySelectorAll(".portfolioBox");
-  for (var i=0; i < portfolioBoxes.length; i++) {
-    portfolioBoxes[i].addEventListener('mouseover', openPortfolioBox)
-    portfolioBoxes[i].addEventListener('mouseout', closePortfolioBox)
+  for (let i=0; i < portfolioBoxes.length; i++) {
+    portfolioBoxes[i].addEventListener('mouseover', openPortfolioBox);
+    portfolioBoxes[i].addEventListener('mouseleave', closePortfolioBox);
   }
 }, 1000)
