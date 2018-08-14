43d0f3229ef1b1de654784f0aaa953a30e75defd
Austin <stnmonroe@gmail.com>
2018-08-13 23:27:16 -0500

    got some animation working for the open door look

--- script.js
+++ script.js
@@ -44,19 +44,25 @@ var portfolioItems = [
     img: {
       name: "Voyager",
       src: "img/voyager.png"
-    }
+    },
+    tech: ["React-Native"],
+    desc: "Mobile application for iOS and Android offering you the chance to compete at hilarity."
   },
   {
     img: {
       name: "Witwars Admin",
       src: "img/witwars_admin.png"
-    }
+    },
+    tech: ["React-Native"],
+    desc: "Mobile application for iOS and Android offering you the chance to compete at hilarity."
   },
   {
     img: {
       name: "Ottawa Recreation",
       src: "img/ottawa_rec.png"
-    }
+    },
+    tech: ["React-Native"],
+    desc: "Mobile application for iOS and Android offering you the chance to compete at hilarity."
   }
 ]
 
@@ -68,28 +74,59 @@ createPortfolioBoxes = () => {
     div.className = 'portfolioBox';
     div.innerHTML = `
       <div class="img1stHalf" style="background: url(` + pI[i].img.src + `) 0 0"></div>
-      <div class="img2ndHalf" style="background: url(` + pI[i].img.src + `) -125px 0">
+      <div class="img2ndHalf" style="background: url(` + pI[i].img.src + `) -125px 0"></div>
+      <div class="portfolioInfo">
+        <p class="portfolioDesc">` + pI[i].desc + `</p>
+        <div class="portfolioTech"></div>
+        <button class="portfolioBtn">View More</button>
+      </div>
     `
+    console.log(div)
     p.appendChild(div);
   }
 }
 
 //Portfolio box open animation
+let portfolioBoxes;
 
-const openPortfolioBox = (i) => {
-  let c = portfolioBoxes[i].children;
-  for (let i = 0; i < c.length; i++) {
-    c.classList.add("reveal");
+const openPortfolioBox = (event) => {
+  var e = event.target || event.srcElement;
+
+  if (e.classList.contains("portfolioInfo") || e.classList.contains("portfolioDesc") ||
+      e.classList.contains("portfolioTech") || e.classList.contains("portfolioBtn")) {
+        return null;
+  } else {
+
+    if (e.classList.contains("img1stHalf") || e.classList.contains("img2ndHalf")) {
+      e = e.parentElement;
+    }
+
+    e.children[0].classList.add("open-left");
+    e.children[1].classList.add("open-right");
+  }
+}
+
+const closePortfolioBox = (event) => {
+  var e = event.target || event.srcElement;
+
+  if (e.classList.contains("portfolioInfo") || e.classList.contains("portfolioDesc") ||
+      e.classList.contains("portfolioTech") || e.classList.contains("portfolioBtn")) {
+        return null;
+  } else {
+
+    if (e.classList.contains("img1stHalf") || e.classList.contains("img2ndHalf")) {
+      e = e.parentElement;
+    }
+
+    e.children[0].classList.remove("open-left");
+    e.children[1].classList.remove("open-right");
   }
 }
 
 setTimeout(() => {
-  var portfolioBoxes = document.getElementsByClassName("portfolioBox");
-
-  console.log(portfolioBoxes.length)
-
+  portfolioBoxes = document.querySelectorAll(".portfolioBox");
   for (var i=0; i < portfolioBoxes.length; i++) {
-    console.log(portfolioBoxes[i])
-    portfolioBoxes[i].addEventListener('mouseover', openPortfolioBox(i));
+    portfolioBoxes[i].addEventListener('mouseover', openPortfolioBox)
+    portfolioBoxes[i].addEventListener('mouseout', closePortfolioBox)
   }
 }, 1000)
